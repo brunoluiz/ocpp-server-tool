ab99d306d4b03306981924525cd74c50abbeabfa
Bruno Luiz da Silva <contato@brunoluiz.net>
2016-11-09 20:52:20 -0200

    Implemented a parameters parser

--- src/main/java/ocpp/cp/commands/RemoteStartTransactionCommand.java
+++ src/main/java/ocpp/cp/commands/RemoteStartTransactionCommand.java
@@ -3,22 +3,45 @@ package ocpp.cp.commands;
 import ocpp.cp.ChargePointCommand;
 import ocpp.cp._2012._06.RemoteStartTransactionRequest;
 import ocpp.cp._2012._06.RemoteStartTransactionResponse;
+import org.apache.commons.cli.*;
 
 /**
  * Created by bds on 09/11/2016.
  */
 public class RemoteStartTransactionCommand extends ChargePointCommand {
-    private String tagId = null;
+    private String tag = null;
     private Integer connector = null;
 
     public RemoteStartTransactionCommand(String parameters) {
         super();
+
+        // create Options object
+        Options options = new Options();
+        Option tag = new Option("t", "tag", true, "tagid");
+        tag.setRequired(true);
+        options.addOption(tag);
+
+        Option connector = new Option("c", "connector", true, "connector id");
+        connector.setRequired(true);
+        options.addOption(connector);
+
+        CommandLineParser parser = new DefaultParser();
+        String[] parametersOptions = parameters.split(" ");
+        CommandLine cmd;
+        try {
+            cmd = parser.parse(options, parametersOptions);
+            this.tag = cmd.getOptionValue("tag");
+            this.connector = Integer.parseInt(cmd.getOptionValue("connector"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public Object execute() throws Exception {
         RemoteStartTransactionRequest parameters = new RemoteStartTransactionRequest();
         parameters.setConnectorId(connector);
-        parameters.setIdTag(tagId);
+        parameters.setIdTag(tag);
 
         RemoteStartTransactionResponse response = chargePointService.remoteStartTransaction(parameters);
         return response;
--- src/main/java/ocpp/cp/commands/RemoteStopTransactionCommand.java
+++ src/main/java/ocpp/cp/commands/RemoteStopTransactionCommand.java
@@ -3,20 +3,37 @@ package ocpp.cp.commands;
 import ocpp.cp.ChargePointCommand;
 import ocpp.cp._2012._06.RemoteStopTransactionRequest;
 import ocpp.cp._2012._06.RemoteStopTransactionResponse;
+import org.apache.commons.cli.*;
 
 /**
  * Created by bds on 09/11/2016.
  */
 public class RemoteStopTransactionCommand extends ChargePointCommand {
-    private Integer transactionId = null;
+    private Integer transaction = null;
 
     public RemoteStopTransactionCommand(String parameters) {
         super();
+
+        // create Options object
+        Options options = new Options();
+        Option tag = new Option("tid", "transaction", true, "transaction id");
+        tag.setRequired(true);
+        options.addOption(tag);
+
+        CommandLineParser parser = new DefaultParser();
+        String[] parametersOptions = parameters.split(" ");
+        CommandLine cmd;
+        try {
+            cmd = parser.parse(options, parametersOptions);
+            this.transaction = Integer.parseInt(cmd.getOptionValue("transaction"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     public Object execute() throws Exception {
         RemoteStopTransactionRequest parameters = new RemoteStopTransactionRequest();
-        parameters.setTransactionId(transactionId);
+        parameters.setTransactionId(transaction);
 
         RemoteStopTransactionResponse response = chargePointService.remoteStopTransaction(parameters);
         return response;
